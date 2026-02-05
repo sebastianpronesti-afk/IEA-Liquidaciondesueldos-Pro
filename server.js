6951d297d1112cc26cd56454cff89ccb3d4cb3ef
sebastianpronesti-afk <sebastianpronesti@gmail.com>
2026-02-05 18:49:34 -0300

    Update HTML file served in root route

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'liquidacion_IEA_v4_30.html'));
+    res.sendFile(path.join(__dirname, 'liquidacion_IEA_v5.html'));
 });
 
 app.use(express.static(__dirname));
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
